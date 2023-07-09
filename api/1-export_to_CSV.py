f890e9b1b4e8326533e49760b8f0681598a5621f
waseprisca0 <117096121+waseprisca0@users.noreply.github.com>
2023-07-09 21:04:37 +0200

    Update 1-export_to_CSV.py

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -1,27 +1,22 @@
 #!/usr/bin/python3
-"""a Python script to export data in the CSV format"""
+"""get TODO list"""
 
+import csv
+import json
 import requests
 import sys
-
 if __name__ == "__main__":
-    employee_id = sys.argv[1]
-    url = "https://jsonplaceholder.typicode.com/users/{}".format(employee_id)
-    todo = "https://jsonplaceholder.typicode.com/todos?userId={}"
-    todo = todo.format(employee_id)
-
-    user_info = requests.request("GET", url).json()
-    todo_info = requests.request("GET", todo).json()
-
-    employee_name = user_info.get("name")
-    employee_username = user_info.get("username")
-    total_tasks = list(filter(lambda x: (x["completed"] is True), todo_info))
-    task_com = len(total_tasks)
-    total_task_done = len(todo_info)
-
-    with open(str(employee_id) + '.csv', "w") as f:
-        [f.write('"' + str(employee_id) + '",' +
-                 '"' + employee_username + '",' +
-                 '"' + str(task["completed"]) + '",' +
-                 '"' + task["title"] + '",' + "\n")
-         for task in todo_info]
+    link = "https://jsonplaceholder.typicode.com/users/{}".format(sys.argv[1])
+    res = requests.get(link)
+    user = json.loads(res.text)
+    num = sys.argv[1]
+    link = "https://jsonplaceholder.typicode.com/users/{}/todos".format(num)
+    res = requests.get(link)
+    todos = json.loads(res.text)
+    csv_data = [["{}".format(i["userId"]),
+                user["username"],
+                "{}".format(i["completed"]),
+                 i["title"]] for i in todos]
+    with open("{}.csv".format(user["id"]), 'w', encoding='utf-8') as f:
+        writer = csv.writer(f, quoting=csv.QUOTE_NONNUMERIC)
+        writer.writerows(csv_data)
